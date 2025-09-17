57410f208ebea749f218eff553ede79e39ba0c5e
kevillyen <kevillyen.carvalho@aluno.senai.br>
2025-09-17 07:20:25 -0300

    operadores

---
+++ operadores/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,25 @@
+﻿// OperadoresAritmeticos
+
+/*
+    + = Adição
+    - = Subtração
+    * = Multiplicação
+    / = Divisão
+*/
+
+int soma = 2 + 2;
+int subtração = 10 - 5;
+int multiplicação = 3 * 7;
+double divisão = 3 / 2.5;
+
+// Utilizando Operadores Aritmeticos com variedades
+int num1, num2, resultado;
+Console.WriteLine("Insira a primeira Nota");
+num1 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Insira a segunda Nota");
+num2 = Convert.ToInt16(Console.ReadLine());
+
+resultado = num1 + num2;
+Console.WriteLine($"A soma das notas é: {resultado}");
+
